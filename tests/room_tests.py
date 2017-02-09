c0c935633d925118cb8e3eb542b2a4bd8d9fb085
andela-iikikin <ichiato.ikikin@andela.com>
2017-02-09 05:42:29 +0100

    add create_office testcase

--- tests/room_tests.py
+++ tests/room_tests.py
@@ -1,9 +1,39 @@
 import unittest
-from app.room import Room
+from app.dojo import Dojo
 
-class TddInPythonExample(unittest.TestCase):
- 
-    def test_calculator_add_method_returns_correct_result(self):
-        calc = Calculator()
-        result = calc.add(2,2)
-        self.assertEqual(4, result)
+class TestDojo(unittest.TestCase):
+    def setUp(self):
+        self.my_dojo = Dojo()
+
+    def clear_all_rooms():
+        Dojo.all_rooms = {}
+
+    def test_create_office(self):
+        TestDojo.clear_all_rooms()
+        initial_room_count = len(self.my_dojo.all_rooms)
+        self.assertEqual(initial_room_count, 0)
+        blue_office = self.my_dojo.create_room('office', ['blue'])
+        self.assertTrue('blue' in self.my_dojo.all_rooms)
+        new_room_count = len(self.my_dojo.all_rooms)
+        self.assertEqual(new_room_count - initial_room_count, 1)
+        another_blue_office = self.my_dojo.create_room('office', ['blue'])
+        self.assertEqual(another_blue_office, 'Room already exist')
+        double_red_offices = self.my_dojo.create_room('office', ['red', 'red'])
+        self.assertEqual(double_red_offices, 'Room already exist')
+        self.assertTrue('red' in self.my_dojo.all_rooms)
+
+    def test_create_ls(self):
+        initial_room_count = len(self.my_dojo.all_rooms)
+        self.assertEqual(initial_room_count, 0)
+        blue_office = self.my_dojo.create_room('living', ['blue'])
+        self.assertTrue('blue' in self.my_dojo.all_rooms)
+        new_room_count = len(self.my_dojo.all_rooms)
+        self.assertEqual(new_room_count - initial_room_count, 1)
+        another_blue_office = self.my_dojo.create_room('living', ['blue'])
+        self.assertEqual(another_blue_office, 'Room already exist')
+        double_red_offices = self.my_dojo.create_room('office', ['red', 'red'])
+        self.assertEqual(double_red_offices, 'Room already exist')
+        self.assertTrue('red' in self.my_dojo.all_rooms)
+
+    def test_add_fellow(self):
+        pass
