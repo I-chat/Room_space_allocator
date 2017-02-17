7a75a7b1a5b6a5b608022fd7c7e57678e17daa66
andela-iikikin <ichiato.ikikin@andela.com>
2017-02-17 16:50:54 +0100

    Add test for print_allocated method

--- tests/room_tests.py
+++ tests/room_tests.py
@@ -1,15 +1,25 @@
 import unittest
+
 from app.dojo import Dojo
+from app.person import Fellow, Staff
 from app.room import Room
-from app.person import Staff, Fellow
+
 
 class TestDojo(unittest.TestCase):
+    longStr = "RED\n" + "------------" \
+        "------------------\n" \
+        "bolaji olajide\n"
+
+    longStr2 = "UNALLOCATED LIST\n" + "---------------" \
+        "---------------\nladi adeniran: fellow Living Space\n" \
+        "bolaji olajide: fellow Living Space\n"
 
     def clear_all_rooms(self):
         Dojo.all_rooms = {}
         Dojo.all_office = []
         Dojo.all_ls = []
-        Dojo.dojo_all_persons = {}
+        Dojo.dojo_persons = {}
+        Dojo.unallocated = {}
 
     def test_create_office(self):
         TestDojo.clear_all_rooms(self)
@@ -46,13 +56,14 @@ class TestDojo(unittest.TestCase):
         my_dojo = Dojo()
         my_dojo.create_room('office', ['blue'])
         ladi_office = my_dojo.all_office[0]
-        self.assertFalse('ladi adeniran' in my_dojo.dojo_all_persons)
-        self.assertFalse('ladi adeniran' in ladi_office.room_all_persons)
+        self.assertFalse('ladi adeniran' in my_dojo.dojo_persons)
+        self.assertFalse('ladi adeniran' in ladi_office.room_persons)
         my_dojo.add_person('ladi', 'adeniran', 'staff')
-        self.assertTrue('ladi adeniran' in my_dojo.dojo_all_persons)
-        self.assertTrue('ladi adeniran' in ladi_office.room_all_persons)
+        self.assertTrue('ladi adeniran' in my_dojo.dojo_persons)
+        self.assertTrue('ladi adeniran' in ladi_office.room_persons)
         answer = my_dojo.add_person('bolaji', 'olajide', 'staff', 'y')
-        self.assertEqual(answer, 'Sorry. Only fellows can have a living space.')
+        self.assertEqual(
+            answer, 'Sorry. Only fellows can have a living space.')
         my_dojo.add_person('oluwadamilola', 'durodola', 'staff')
         my_dojo.add_person('mumeen', 'olasode', 'staff')
         my_dojo.add_person('ichiato', 'ikikin', 'staff')
@@ -68,13 +79,13 @@ class TestDojo(unittest.TestCase):
         my_dojo.create_room('living', ['blue'])
         bj_living = my_dojo.all_ls[0]
         bj_office = my_dojo.all_office[0]
-        self.assertFalse('bolaji olajide' in my_dojo.dojo_all_persons)
-        self.assertFalse('bolaji olajide' in bj_office.room_all_persons)
-        self.assertFalse('bolaji olajide' in bj_living.room_all_persons)
+        self.assertFalse('bolaji olajide' in my_dojo.dojo_persons)
+        self.assertFalse('bolaji olajide' in bj_office.room_persons)
+        self.assertFalse('bolaji olajide' in bj_living.room_persons)
         my_dojo.add_person('bolaji', 'olajide', 'fellow', 'y')
-        self.assertTrue('bolaji olajide' in my_dojo.dojo_all_persons)
-        self.assertTrue('bolaji olajide' in bj_living.room_all_persons)
-        self.assertTrue('bolaji olajide' in bj_office.room_all_persons)
+        self.assertTrue('bolaji olajide' in my_dojo.dojo_persons)
+        self.assertTrue('bolaji olajide' in bj_living.room_persons)
+        self.assertTrue('bolaji olajide' in bj_office.room_persons)
 
     def test_print_room(self):
         my_dojo = Dojo()
@@ -83,37 +94,54 @@ class TestDojo(unittest.TestCase):
         my_dojo.create_room('living', ['blue'])
         my_dojo.add_person('bolaji', 'olajide', 'staff', 'y')
         answer = my_dojo.print_room('red')
-        self.assertEqual(answer, 'There are no occupants in red at the moment.')
+        self.assertEqual(
+            answer, 'There are no occupants in red at the moment.')
         answer = my_dojo.print_room('blue')
-        self.assertEqual(answer, 'There are no occupants in blue at the moment.')
+        self.assertEqual(
+            answer, 'There are no occupants in blue at the moment.')
         my_dojo.add_person('ladi', 'adeniran', 'fellow')
         answer = my_dojo.print_room('red')
         self.assertEqual(answer, 'ladi adeniran --> fellow')
         answer = my_dojo.print_room('blue')
-        self.assertEqual(answer, 'There are no occupants in blue at the moment.')
-
-    def test_file_print_allocations(self):
-        my_dojo = Dojo()
-        TestDojo.clear_all_rooms(self)
-        my_dojo.create_room('office', ['red'])
-        my_dojo.add_person('bolaji', 'olajide', 'fellow', 'y')
-        longStr = "RED\n" + "---------------" \
-                "----------------" \
-                "-------------------\n" \
-                "bolaji olajide\n"
-        my_dojo.print_allocations('output')
-        my_file = open('data/output.txt', 'r')
-        content = my_file.read()
-        my_file.close()
-        self.assertEqual(content, longStr)
+        self.assertEqual(
+            answer, 'There are no occupants in blue at the moment.')
 
     def test_print_allocations(self):
         my_dojo = Dojo()
         TestDojo.clear_all_rooms(self)
         my_dojo.create_room('office', ['red'])
         my_dojo.add_person('bolaji', 'olajide', 'fellow')
-        longStr = "RED\n" + "-----------------" \
-                "-----------------------\n" \
-                "bolaji olajide\n"
         content = my_dojo.print_allocations()
-        self.assertEqual(content, longStr)
+        self.assertEqual(content, self.longStr)
+
+    def test_file_print_allocations(self):
+        my_dojo = Dojo()
+        TestDojo.clear_all_rooms(self)
+        my_dojo.create_room('office', ['red'])
+        my_dojo.add_person('bolaji', 'olajide', 'fellow', 'y')
+        my_dojo.print_allocations('output')
+        my_file = open('data/output.txt', 'r')
+        content = my_file.read()
+        my_file.close()
+        self.assertEqual(content, self.longStr)
+
+    def test_file_print_unallocations(self):
+        my_dojo = Dojo()
+        TestDojo.clear_all_rooms(self)
+        my_dojo.create_room('office', ['red'])
+        my_dojo.add_person('bolaji', 'olajide', 'fellow', 'y')
+        my_dojo.add_person('ladi', 'adeniran', 'fellow', 'y')
+        my_dojo.print_unallocated('test_output')
+        my_file = open('data/test_output.txt', 'r')
+        content = my_file.read()
+        my_file.close()
+        self.assertEqual(content, self.longStr2)
+
+    def test_print_unallocated(self):
+        my_dojo = Dojo()
+        TestDojo.clear_all_rooms(self)
+        my_dojo.create_room('office', ['red'])
+        my_dojo.add_person('bolaji', 'olajide', 'fellow', 'y')
+        my_dojo.add_person('ladi', 'adeniran', 'fellow', 'y')
+        content = my_dojo.print_unallocated()
+        self.assertEqual(content, self.longStr2)
