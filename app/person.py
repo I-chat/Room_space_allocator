850dba99871f881adf55fba123aae706c4b95c48
andela-iikikin <ichiato.ikikin@andela.com>
2017-02-17 18:00:37 +0100

    Add person, staff, fellow class

---
+++ app/person.py
@@ -0,0 +1,23 @@
+class Person(object):
+    """Creates a Person object"""
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.full_name = (first_name + ' ' + last_name)
+
+
+class Staff(Person):
+    """Creates a Staff object that inherits properties from the Person class"""
+
+    def __init__(self, first_name, last_name):
+        super(Staff, self).__init__(first_name, last_name)
+        self.person_type = 'staff'
+
+
+class Fellow(Person):
+    """Creates a Fellow object that inherits properties from the Person class"""
+
+    def __init__(self, first_name, last_name):
+        super(Fellow, self).__init__(first_name, last_name)
+        self.person_type = 'fellow'
