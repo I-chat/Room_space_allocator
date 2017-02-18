f9a2dcb07ec6b997e987b5f3dcf19675a68b36c4
andela-iikikin <ichiato.ikikin@andela.com>
2017-02-18 07:20:43 +0100

    Fix double room bug in create_room

--- app/dojo.py
+++ app/dojo.py
@@ -10,25 +10,25 @@ class Dojo(object):
     unallocated = {}
 
     def create_room(self, room_type, room_name):
+        output = ""
         for room in room_name:
-            output = ""
             if room in Dojo.all_rooms:
-                output = output + ('Room already exist\n')
+                output = output + ('Room already exist.\n')
             elif not room.isalnum():
-                output = output + ('Invalid room naming convention!\n')
+                output = output + ('Invalid room naming convention!.\n')
             elif room_type == 'office':
                 new_room = Office(room)
                 self.all_office.append(new_room)
                 self.all_rooms[new_room.room_name] = new_room.room_type
                 output = output + \
-                    ('An office called %s has been created\n.' % room)
+                    ('An office called %s has been created.\n' % room)
             elif room_type == 'living':
                 new_room = LivingSpace(room)
                 self.all_ls.append(new_room)
                 self.all_rooms[new_room.room_name] = new_room.room_type
                 output = output + \
-                    ('A living space called %s has been created\n.' % room)
+                    ('A living space called %s has been created.\n' % room)
             elif room_type != 'living' and room_type != 'office':
                 output = output + ('Invalid command.\nFirst argument must be either'
-                                   'an office or living\n')
+                                   'an office or living.\n')
         return output
